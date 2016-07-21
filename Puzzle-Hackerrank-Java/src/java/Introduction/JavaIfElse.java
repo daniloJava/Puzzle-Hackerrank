c4471255bec8cfe0d60fa55d996f262823871fb8
Danilo Silva <danilo.manoel_oliveira@hotmail.com>
2016-07-20 23:58:17 -0300

    3- Java If-Else

--- Puzzle-Hackerrank-Java/src/java/Introduction/JavaIfElse.java
+++ Puzzle-Hackerrank-Java/src/java/Introduction/JavaIfElse.java
@@ -4,14 +4,11 @@ import java.util.Scanner;
 
 /**
  * Task Given an integer, , perform the following conditional actions:
- * 
  * If is odd, print Weird If is even and in the inclusive range of to , print
  * Not Weird If is even and in the inclusive range of to , print Weird If is
  * even and greater than , print Not Weird Complete the stub code provided in
  * your editor to print whether or not is weird.
- * 
  * Input Format
- * 
  * A single line containing a positive integer, .
  * 
  * @author tapower
