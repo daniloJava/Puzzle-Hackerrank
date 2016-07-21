a276eb9ef3737d3ccecaa70c12f9a41222712bfc
Danilo Silva <danilo.manoel_oliveira@hotmail.com>
2016-07-21 00:21:05 -0300

    3 - Java Stdin and Stdout II
    Cuidado com o Scanner
    recupere a linha com NextLine() depois converta

---
+++ Puzzle-Hackerrank-Java/src/java/Introduction/JavaStdinAndStdoutII.java
@@ -0,0 +1,39 @@
+package java.Introduction;
+
+import java.util.Scanner;
+
+/**
+ * Input Format
+ * 
+ * There are three lines of input:
+ * 
+ * The first line contains an integer. The second line contains a double. The
+ * third line contains a String. Output Format
+ * 
+ * There are three lines of output:
+ * 
+ * On the first line, print String: followed by the unaltered String read from
+ * stdin. On the second line, print Double: followed by the unaltered double
+ * read from stdin. On the third line, print Int: followed by the unaltered
+ * integer read from stdin. To make the problem easier, a portion of the code is
+ * already provided in the editor.
+ * 
+ * @author tapower
+ *
+ */
+public class JavaStdinAndStdoutII {
+	
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = Integer.parseInt(scan.nextLine());
+        double d = Double.parseDouble(scan.nextLine());
+        String s = scan.nextLine();
+        
+        // Write your code here.
+        
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+
+}
