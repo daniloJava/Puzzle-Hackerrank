6dc902fed57c417e2c5ef4d2c777e09b2b7ebeeb
Danilo Silva <danilo.manoel_oliveira@hotmail.com>
2016-07-20 23:57:59 -0300

    1- Welcome to Java!

--- Puzzle-Hackerrank-Java/src/java/Introduction/WelcomeToJava.java
+++ Puzzle-Hackerrank-Java/src/java/Introduction/WelcomeToJava.java
@@ -2,17 +2,10 @@ package java.Introduction;
 
 /**
  * Problema Input Format
- * 
  * There is no input for this challenge.
- * 
  * Output Formatw
- * 
  * You must print two lines of output:
- * 
  * Print Hello, World. on the first line. Print Hello, Java. on the second line.
- * 
-
- * 
  */
 public class WelcomeToJava {
 
