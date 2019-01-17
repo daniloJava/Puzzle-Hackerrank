c0ab76c9e2032fd9e33efa4a04319434c9e97dc5
Danilo Silva <dmanoel@magnasistemas.com.br>
2019-01-17 21:18:23 -0200

    Java Substring

---
+++ Puzzle-Hackerrank-Java/src/StringsChallenges/JavaSubstring.java
@@ -0,0 +1,17 @@
+package StringsChallenges;
+
+import java.util.Scanner;
+
+public class JavaSubstring {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String S = in.next();
+		int start = in.nextInt();
+		int end = in.nextInt();
+		in.close();
+
+		System.out.println(S.substring(start, end));
+
+	}
+}
