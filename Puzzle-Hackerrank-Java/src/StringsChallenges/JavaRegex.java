6d758230da1f762675faa171589b5ad35fc7f302
Danilo Silva <dmanoel@magnasistemas.com.br>
2019-01-20 15:07:23 -0200

    JavaRegex

---
+++ Puzzle-Hackerrank-Java/src/StringsChallenges/JavaRegex.java
@@ -0,0 +1,21 @@
+package StringsChallenges;
+
+import java.util.Scanner;
+
+public class JavaRegex {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNext()) {
+			String IP = in.next();
+			System.out.println(IP.matches(new MyRegex().pattern));
+		}
+
+	}
+}
+
+class MyRegex {
+
+	String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+	public String pattern = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
+
+}
